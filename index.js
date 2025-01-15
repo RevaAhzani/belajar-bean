7c07ab2d55ef5d3471dad32a052a90d4e476113e
RevaAhzani <reva7021012@gmail.com>
2025-01-15 12:56:36 +0700

    lagi14

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const dotenv = require('dotenv');
 const port = process.env.PORT || 3000;
 dotenv.config();
 app.get('/', (req, res) => {
-  res.send('Hello World!, Coba Elastbeanstalk');
+  res.send('Hello World!, Coba Elastbeanstalk, ini yang terbaru');
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
